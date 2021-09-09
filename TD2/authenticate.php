faccc59fb95cbb68c34e78273b173fb6231218e2
Wilt Lilian <lilian.wilt@hotmail.com>
2021-09-09 11:22:55 +0200

    Exo 1 fini et début exo2

--- TD2/authenticate.php
+++ TD2/authenticate.php
@@ -1,12 +1,16 @@
 <?php
 
+include 'users.php';
 
-if ( isset($_POST['login']) && isset($_POST['password']) )
-{
-    $login = htmlentities($_POST['login']);
-    $password = htmlentities($_POST['password']);
-
-    
+$meth = $_SERVER['REQUEST_METHOD'];
+if($meth=='POST') {
+    if (isset($_POST['login']) && isset($_POST['password'])) {
+        $login = htmlentities($_POST['login']);
+        $password = htmlentities($_POST['password']);
+    }
+}else{
+    header('Location: signin.php');
+    exit();
 }
 
 exit;
--- TD2/signin.php
+++ TD2/signin.php
@@ -6,8 +6,17 @@
 	</head>
 	<body>
 		<h1>Signin</h1>
-		<form action="authenticate.php">
-			<input type="submit" value="Connect me">
-		</form>
+        <?php
+
+        $meth = $_SERVER['REQUEST_METHOD'];
+        if($meth=='GET') {
+            echo '<form action = "authenticate.php" >
+                    <p > Votre identifiant : <input type = "text" name = "identifiant" ></p >
+                    <p > Votre mot de passe : <input type = "password" name = "mdp" ></p >
+			        <input type = "submit" value = "Connect " >
+		          </form >'
+            ;
+		}
+		?>
 	</body>
 </html>
